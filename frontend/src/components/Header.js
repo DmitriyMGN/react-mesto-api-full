d397ab783576c50ad0e5511e78b46a04ec81ffbf
Dmitriy <dmitry.mysyagin@yandex.ru>
2022-09-28 19:04:49 +0300

    routes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -8,8 +8,8 @@ function Header (props) {
   <header className="header">
     <img src={mestoLogo} alt="Картинка логотипа Место" className="header__logo" />
     <div className="navbar">
-      {location.pathname === '/signin' && <Link className="navbar__element" to="/sign-up">Регистрация</Link> }
-      {location.pathname === '/signup' && <Link className="navbar__element" to="/sign-in">Войти</Link> }
+      {location.pathname === '/signin' && <Link className="navbar__element" to="/signup">Регистрация</Link> }
+      {location.pathname === '/signup' && <Link className="navbar__element" to="/signin">Войти</Link> }
       {location.pathname === '/' && <p className="navbar__element">{props.email}</p> }
       {location.pathname === '/' && <button onClick={props.signOut} className="navbar__element navbar__element_type_button">Выйти</button> }
     </div>
