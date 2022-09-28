7a28c99c4ef9b90b81cff9f770d641abe9c9af9d
Dmitriy <dmitry.mysyagin@yandex.ru>
2022-09-28 20:15:44 +0300

    autor

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,14 +1,16 @@
 const jwt = require('jsonwebtoken');
 const { AutorizationError } = require('../errors/AutorizationError');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 const auth = async (req, res, next) => {
   const token = req.cookies.jwt;
   let payload;
 
   try {
-    payload = await jwt.verify(token, 'SECRET');
+    payload = await jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    return next(new AutorizationError('Необходима авторизация'));
+    return next(new AutorizationError('Необходима авторизация!'));
   }
 
   req.user = payload;
