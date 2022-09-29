a1840bd1f054b0a993617846a52c3b25cbc3c09f
Dmitriy <dmitry.mysyagin@yandex.ru>
2022-09-29 08:52:55 +0300

    fix

--- backend/app.js
+++ backend/app.js
@@ -37,7 +37,7 @@ app.post(
   celebrate({
     body: Joi.object().keys({
       email: Joi.string().required().email(),
-      password: Joi.string().required().min(8),
+      password: Joi.string().required(),
     }),
   }),
   login,
