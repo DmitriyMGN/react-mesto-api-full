e27ae8bfa19e09dc27e8707200a8b8c5204fa5ce
Dmitriy <dmitry.mysyagin@yandex.ru>
2022-09-29 11:42:30 +0300

    reg

--- backend/app.js
+++ backend/app.js
@@ -50,7 +50,7 @@ app.post(
       password: Joi.string().required(),
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
-      avatar: Joi.string().regex(/^((ftp|http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!-])*)?/g),
+      avatar: Joi.string().regex(),
     }),
   }),
   createUser,
